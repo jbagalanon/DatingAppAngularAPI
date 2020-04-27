97ad8c3e6f0c089b81af6d331c882f91b5186a8a
Joemar <jbagalanon@gmail.com>
2020-04-27 20:49:17 +0800

    added iteration of database file, and add http module

--- src/app/value/value.component.ts
+++ src/app/value/value.component.ts
@@ -1,15 +1,28 @@
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { analyzeAndValidateNgModules } from '@angular/compiler';
+import { AnonymousSubject } from 'rxjs/internal/Subject';
 
 @Component({
   selector: 'app-value',
   templateUrl: './value.component.html',
-  styleUrls: ['./value.component.css']
+  styleUrls: ['./value.component.css'],
 })
 export class ValueComponent implements OnInit {
+  values: any;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.getValues();
   }
 
+  getValues() {
+    this.http.get('https://localhost:44399/api/values').subscribe(
+      (response) => {
+        this.values = response;
+      },
+      (error) => console.log(error)
+    );
+  }
 }
