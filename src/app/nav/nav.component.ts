cb4105c28540d482c14b0748b6ab66b72092be30
Joemar <jbagalanon@gmail.com>
2020-05-09 03:15:32 +0800

    added alertjs conditions and modify console.log error reporting

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,3 +1,4 @@
+import { AlertifyService } from './../_services/alertify.service';
 import { AuthService } from '../_services/auth.service';
 import { Component, OnInit } from '@angular/core';
 
@@ -9,17 +10,20 @@ import { Component, OnInit } from '@angular/core';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private alertify: AlertifyService
+  ) {}
 
   ngOnInit() {}
 
   login() {
     this.authService.login(this.model).subscribe(
       (next) => {
-        console.log('Logged in successfully');
+        this.alertify.success('Logged in successfully');
       },
       (error) => {
-        console.log();
+        this.alertify.error();
       }
     );
   }
@@ -30,6 +34,6 @@ export class NavComponent implements OnInit {
   }
   loggedOut() {
     localStorage.removeItem('token');
-    console.log('logged out');
+    this.alertify.message('logged out');
   }
 }
