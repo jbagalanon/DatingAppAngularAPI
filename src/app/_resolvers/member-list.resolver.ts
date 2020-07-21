fa21959bee1b82444c874fce121da33a0122c02e
Joemar <jbagalanon@gmail.com>
2020-07-21 22:57:35 +0800

    Modify member-list resolverdone

--- src/app/_resolvers/member-list.resolver.ts
+++ src/app/_resolvers/member-list.resolver.ts
@@ -1,24 +1,24 @@
 import { Router, Resolve, ActivatedRouteSnapshot } from '@angular/router';
 import { Injectable } from '@angular/core';
-import { AlertifyService } from './../_services/alertify.service';
-import { UserService } from './../_services/user.service';
+import { AlertifyService } from '../_services/alertify.service';
+import { UserService } from '../_services/user.service';
 import { User } from '../_models/user';
 import { Observable, of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
 @Injectable()
-export class MemberDetailResolver implements Resolve<User> {
+export class MemberListResolver implements Resolve<User[]> {
   constructor(
     private userService: UserService,
     private router: Router,
     private alertify: AlertifyService
   ) {}
 
-  resolve(route: ActivatedRouteSnapshot): Observable<User> {
-    return this.userService.getUser(route.params['id']).pipe(
+  resolve(route: ActivatedRouteSnapshot): Observable<User[]> {
+    return this.userService.getUsers().pipe(
       catchError((error) => {
         this.alertify.error('Problem retrieving data');
-        this.router.navigate(['/members']);
+        this.router.navigate(['/home']);
         return of(null);
       })
     );
