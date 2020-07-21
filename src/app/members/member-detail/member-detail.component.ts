e8f944885b0d1f30cdc25d72ccd49b6e7df19ff7
Joemar <jbagalanon@gmail.com>
2020-07-22 01:25:36 +0800

    configuring galley in member detail components

--- src/app/members/member-detail/member-detail.component.ts
+++ src/app/members/member-detail/member-detail.component.ts
@@ -3,6 +3,12 @@ import { UserService } from './../../_services/user.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { User } from '../../_models/user';
+import {
+  NgxGalleryOptions,
+  NgxGalleryImage,
+  NgxGalleryAnimation,
+} from 'ngx-gallery';
+import { ThrowStmt } from '@angular/compiler';
 
 @Component({
   selector: 'app-member-detail',
@@ -11,6 +17,8 @@ import { User } from '../../_models/user';
 })
 export class MemberDetailComponent implements OnInit {
   user: User;
+  galleryOptions: NgxGalleryOptions[];
+  galleryImages: NgxGalleryImage[];
 
   constructor(
     private userService: UserService,
@@ -22,17 +30,33 @@ export class MemberDetailComponent implements OnInit {
     this.route.data.subscribe((data) => {
       this.user = data['user'];
     });
+
+    this.galleryOptions = [
+      {
+        width: '500px',
+        height: '500px',
+        imagePercent: 100,
+        thumbnailsColumns: 4,
+        imageAnimation: NgxGalleryAnimation.Slide,
+        preview: false,
+      },
+    ];
+
+    this.galleryImages = this.getImages();
   }
 
-  // // this.userService.getUser(this.route.snapshot.params['id']) supposedly this the code
-  // loadUser() {
-  //   this.userService.getUser(+this.route.snapshot.params.id).subscribe(
-  //     (user: User) => {
-  //       this.user = user;
-  //     },
-  //     (error) => {
-  //       this.alertify.error(error);
-  //     }
-  //   );
-  // }
+  getImages() {
+    const imageUrls = [];
+
+    // tslint:disable-next-line: prefer-for-of
+    for (let i = 0; i < this.user.photos.length; i++) {
+      imageUrls.push({
+        small: this.user.photos[i].url,
+        medium: this.user.photos[i].url,
+        big: this.user.photos[i].url,
+        description: this.user.photos[i].description,
+      });
+    }
+    return imageUrls;
+  }
 }
