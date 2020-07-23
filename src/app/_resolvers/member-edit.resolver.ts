afb3dadb00a7011f5864bdcb119a00d4b1f8ffda
Joemar <jbagalanon@gmail.com>
2020-07-23 12:53:55 +0800

    modify memmbereditresolver and component

--- src/app/_resolvers/member-edit.resolver.ts
+++ src/app/_resolvers/member-edit.resolver.ts
@@ -19,7 +19,7 @@ export class MemberEditResolver implements Resolve<User> {
   resolve(route: ActivatedRouteSnapshot): Observable<User> {
     return this.userService.getUser(this.authService.decodedToken.nameid).pipe(
       catchError((error) => {
-        this.alertify.error('Problem retrieving data');
+        this.alertify.error('Problem retrieving your data');
         this.router.navigate(['/members']);
         return of(null);
       })
